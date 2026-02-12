373607383f365355577d4ddcc2f69e1695acba7c
Alejandro <alejandroburruezo@gmail.com>
2026-02-12 17:24:21 +0100

    Creación del endpoint y página

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
+from flask import Flask, jsonify
 from bayeta import frotar
-print("Hola, mundo")
+
+app = Flask(__name__)
+
+@app.route('/')
+def hola_mundo():
+    return "<h1>Hola, mundo</h1>"
+
+@app.route('/frotar/<int:n_frases>')
+def endpoint_frotar(n_frases):
+    frases = frotar(n_frases)
+    return jsonify({"frases": frases})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- bayeta.py
+++ bayeta.py
@@ -1,4 +1,5 @@
 # bayeta.py
 
 def frotar(n_frases: int = 1) -> list:
-    pass
+    # Tu solución temporal de "Experto en Web"
+    return ["Mi codigo fuciona a la primera (spoiler, no)" for _ in range(n_frases)]
